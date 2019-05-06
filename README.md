1dd0d73c98e30fb01c9b1a54135f5faae97f5864
mikol <mikol.aspinwall@gmail.com>
2019-05-06 18:11:29 -0400

    yeah, it broke everything

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Reacter
 A microblogging service, built on ReactJS and Flask. 
+
+## How to Edit
+I recognize that this has quickly become a humongous project, but there are only a couple files actually worth looking at. They are:
+- `reacter_web/templates/`
+  - This folder holds all of the HTML files, all your HTML work will happen here. Any new HTML files should go in this directory.
+- `reacter_web/static/`
+  - All static files go here, that includes CSS, JS, anything that's not HTML. I've already set up links between the templates files and the respective static files, so don't worry about that, stuff should just work. As of writing this, I haven't created any JavaScript scripts, but I have already included React dependencies inside all the HTML, so any React you write will just work. In order to link to this from within an HTML, since we're using Flask, you'll have to use `href="{{ url_for('static', filename='file.js') }}"` as opposed to the traditional `href='static/file.js'`. It's a whole thing, I know. Such is the price of making Reacter.
+- `__init__.py`
+  - This project has a really bad file structure which isn't right, and you should **never** have a whole bunch of things inside the `__init__.py` file. Well, we do, and I'll try to fix this soon. Anyways, for the time being, `__init__.py` is where all the Flask code is and you're welcome to take a look around and change things.
+
+## How to Use
+- In order to run this, you've gotta have Flask, and it all runs in a virtual environment. For first time setup, be sure to install Flask. This is the command you have to run in order to get it running on your local machine, while in the directory with `__init__.py`. Eventually, I'll get a virtual machine to run this on so you don't have to worry about running it yourself. 
+```
+FLASK_APP=__init__.py flask run
+```
+
+### If you can't get it working
+well sucks i don't even know how to get it working either lol
--- reacter_web/__init__.py
+++ reacter_web/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -22,12 +22,44 @@ def create_app(test_config=None):
     @app.route("/")
     # This is going to be the homepage, nothing much should have to happen here
     def homepage():
+        # check for cookie
+        # if logged in cookie exists:
+              # return feed
+        # if it does not exist, do this:
         return render_template('index.html')
+
+    @app.route("/testingfeed")
+    def feed():
+        # this definitely needs to be more complicated, or maybe not(?)
+        return render_template('feed.html')
 
     @app.route("/user/<userid>")
     # This is the user profile page
     def profile(userid):
         return render_template('profile.html', userid=userid)
+
+    @app.route("/register", methods=['GET', 'POST'])
+    def register():
+        if request.method == "POST":
+            # handle = request.form['username']
+            # password = request.form['password']
+            # so, we'll need to catch the username and password data here to put into the database
+            # that's done with requests.get(index of thing) and db.execute(
+            #    'INSERT INTO users (handle, password) VALUES (?, ?)',
+            #    (handle, password)
+            #)
+            # STORING PASSWORD IN PLAIN TEXT IS BAD PRACTICE ^^^^^^
+            # as well as check if that user already exists in the database
+            # could this be done clientside? ^ [yes it can be, by AJAX protocol which is implementable in JavaScript]
+            # all data verification stuff should be done by javascript (essentially making sure that username and password aren't empty)
+            return render_template('profile.html', userid=userid)
+        else:
+            return render_template('register.html')
+
+    @app.route("/post/<postid>")
+    def getPost(postid):
+        return render_template('post.html')
+        # return render_template('post.html', postData=postData), but we need to make postData first
     
     from . import db
     db.init_app(app)
